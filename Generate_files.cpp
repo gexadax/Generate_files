be08040dd9055d3b8dfe440221519c15d96b67bc
Андрей Курбанов <kurban1111@mail.ru>
2023-04-28 19:54:00 +0300

    Add const

--- Generate_files.cpp
+++ Generate_files.cpp
@@ -6,3 +6,11 @@
 #include <cstdlib>
 #include <ctime>
 #include <queue>
+
+
+
+const string FILE_NAME = "numbers.txt";
+const int CHUNK_SIZE = 1000000; // размер фрагмента файла
+const int MAX_CHUNKS = 100; // максимальное количество фрагментов
+const string TEMP_FILE_PREFIX = "temp_chunk_"; // префикс имени временного файла
+const int MAX_NUM = 1000000; // максимальное значение числа
